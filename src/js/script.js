e50c5433ecf250c5c29b8c4971a1c2611e05e6e6
yusufbek_orzibekov <yusufbek_orzibekov@student.itpu.uz>
2024-12-20 02:55:08 +0500

    Add z-index adjustments and toggle functionality for mobile navigation

--- src/js/script.js
+++ src/js/script.js
@@ -33,3 +33,16 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+const burger = document.querySelector('.header-burger');
+const nav = document.querySelector('.header__nav');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('open');
+});
+
+link.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('open');
+    });
+});
